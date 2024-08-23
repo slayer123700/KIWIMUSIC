9b5222690613106b23b9054c8b4f8b9e76914169
Destroyerofworldd <destroyerofworld008@gmail.com>
2024-08-23 13:08:09 +0530

    Update couples.py

--- PURVIMUSIC/plugins/tools/couples.py
+++ PURVIMUSIC/plugins/tools/couples.py
@@ -14,8 +14,8 @@ from PURVIMUSIC.mongo.couples_db import _get_image, get_couple
 POLICE = [
     [
         InlineKeyboardButton(
-            text="|| ˹𖤍 𝗔𝗟𝗣𝗛𝗔 𖤍˼ ||",
-            url=f"https://t.me/PURVI_UPDATES",
+            text="𓆩🇩𝙚𝙨𝙩𝙧𝙤𝙮𝙚𝙧𓆪",
+            url=f"https://t.me/Avengers_net_work",
         ),
     ],
 ]
