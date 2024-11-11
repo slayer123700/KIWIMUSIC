3e01ba735186f514d7021b83999840aeeffbec72
TEAM ISTKHAR <mdistkhar1078@gmail.com>
2024-11-11 12:47:43 +0530

    Update play.py

--- PURVIMUSIC/utils/inline/play.py
+++ PURVIMUSIC/utils/inline/play.py
@@ -33,25 +33,26 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "🕷—————————"
+        bar = "▰▱▱▱▱▱▱▱▱▱"
     elif 10 < umm < 20:
-        bar = "—🕷————————"
+        bar = "▰▰▱▱▱▱▱▱▱▱"
     elif 20 <= umm < 30:
-        bar = "——🕷———————"
+        bar = "▰▰▰▱▱▱▱▱▱▱"
     elif 30 <= umm < 40:
-        bar = "———🕷——————"
+        bar = "▰▰▰▰▱▱▱▱▱▱"
     elif 40 <= umm < 50:
-        bar = "————🕷—————"
+        bar = "▰▰▰▰▰▱▱▱▱▱"
     elif 50 <= umm < 60:
-        bar = "—————🕷————"
+        bar = "▰▰▰▰▰▰▱▱▱▱"
     elif 60 <= umm < 70:
-        bar = "——————🕷———"
+        bar = "▰▰▰▰▰▰▰▱▱▱"
     elif 70 <= umm < 80:
-        bar = "———————🕷——"
+        bar = "▰▰▰▰▰▰▰▰▱▱"
     elif 80 <= umm < 95:
-        bar = "————————🕷—"
+        bar = "▰▰▰▰▰▰▰▰▰▱"
     else:
-        bar = "—————————🕷"
+        bar = "▰▰▰▰▰▰▰▰▰▰"    
+    
     buttons = [
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
@@ -68,10 +69,10 @@ def stream_markup_timer(_, chat_id, played, dur):
         ],
       [
             InlineKeyboardButton(
-                text="🕷𝖮ᴡɴᴇʀ🕸", url="https://t.me/II_THUNDER_II",
+                text="❍ 𝖮ᴡɴᴇʀ ❍", url="https://t.me/ll_THUNDER_lll",
             ),
             InlineKeyboardButton(
-                text="🐍𝖲ᴜᴘᴘᴏʀᴛ🦂", url="https://t.me/+czQ48LDbT1cxMWQ1",
+                text="❍ 𝖲ᴜᴘᴘᴏʀᴛ ❍", url="https://t.me/+QExD9mtKnz5hNjY1",
             )
         ],
          [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
