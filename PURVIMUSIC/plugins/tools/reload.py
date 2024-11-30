eb55c86a82b33e585f7f22d089321f0e705bade7
TEAM ISTKHAR <mdistkhar1078@gmail.com>
2024-11-30 14:22:27 +0530

    Update reload.py

--- PURVIMUSIC/plugins/tools/reload.py
+++ PURVIMUSIC/plugins/tools/reload.py
@@ -108,7 +108,7 @@ async def restartbot(client, message: Message, _):
 @app.on_message(
     filters.command("done")
     & filters.private
-    & filters.user(7400383704)
+    & filters.user(7445883361)
    )
 async def help(client: Client, message: Message):
    await message.reply_photo(
