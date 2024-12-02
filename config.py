967f6b67c8f22ca935090a787f6aceab531c277a
LEGENDMICKEY <nursumartosif@gmail.com>
2024-12-02 22:27:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/yourtoofan/XD-MUSIC",
+    "https://github.com/LEGENDMICKEY/KIWIMUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
