b7fbb684c09650a2cb5ab2e573a3c49e5fd14f00
Legend mickey <toofanpapa424@gmail.com>
2024-12-02 19:07:44 +0530

    Update wishcute.py

--- PURVIMUSIC/plugins/yumi/wishcute.py
+++ PURVIMUSIC/plugins/yumi/wishcute.py
@@ -4,7 +4,7 @@ import random
 import requests
 from PURVIMUSIC import app 
 
-SUPPORT_CHAT = "PURVI_UPDATES"
+SUPPORT_CHAT = "THE_INCRICIBLE"
 
 @app.on_message(filters.command("wish"))
 async def wish(_, m):
